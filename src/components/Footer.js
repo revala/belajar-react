773cca5bf026f585f2dafdfda70073cae1ab92b1
revala <revalainda19@gmail.com>
2022-12-04 14:36:04 +0700

    fixingg

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+const Footer = () => {
+    return (
+        // <div>
+        // <footer class="bg-light text-center text-lg-start">
+        
+        // <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        //     © 2020 Copyright:
+        //     <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        // </div>
+        
+        // </footer>
+        // </div>
+        <h1 class="text-center">&copy; 2022</h1>
+    );
+  }
+  export default Footer;
